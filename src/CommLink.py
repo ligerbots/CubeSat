56f802b9d410c592e92d8fece4b2626a1a28161c
Paul Rensing <prensing@ligerbots.org>
2022-05-03 20:19:46 -0400

    Add a CommLink class to handle the bluetooth

---
+++ src/CommLink.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+class CommLink:
+    '''Handle the comm link to the base station (bluetooth)'''
+
+    def __init__(self):
+        return
+
+    def is_comm_up(self):
+        '''Test if communications are up'''
+        return
+
+    def send(self, file):
+        '''Send a file over the comm link. Return True is successful'''
+        return
